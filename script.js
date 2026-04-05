f1747685c50bf2da70caa63d3d0ad8a04aaa2ff3
nine79796 <mohamedtoure2049@gmail.com>
2026-04-06 01:42:09 +0200

    second

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 fetch("/api/firebase")
   .then(res => res.json())
-  .then(data => {
-    console.log("Données récupérées :", data);
-    // Ici tu peux mettre à jour le DOM
-    // ex: document.getElementById('result').textContent = JSON.stringify(data);
-  });
+  .then(data => console.log("Données récupérées :", data))
+  .catch(err => console.error("Erreur fetch API :", err));
