086ce8b8994620c14f0182dd53baf35976a5e68d
nine <130987027+nine79796@users.noreply.github.com>
2026-04-06 14:59:27 +0200

    Delete api directory

--- api/firebase.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const API_KEY = process.env.FIREBASE_API_KEY;
-
-  // Exemple simple pour tester
-  res.status(200).json({ message: "Clé sécurisée côté serveur", keyLength: API_KEY.length });
-}
