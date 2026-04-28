0659d786a4a529a2976b876a2002ae7b188290b5
nine <130987027+nine79796@users.noreply.github.com>
2026-04-28 09:48:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //  ⚠️  Ne jamais intercepter les requêtes Firebase/Firestore
 // ════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'lexique-v6';
+const CACHE_VERSION = 'lexique-v7';
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC = `${CACHE_VERSION}-dynamic`;
 
